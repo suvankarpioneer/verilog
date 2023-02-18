d001a61e6abd6eb526bc85f51d7dbd0a01779c24
suvankarpioneer <58272587+suvankarpioneer@users.noreply.github.com>
2023-02-18 23:55:15 +0530

    Update p2s.v

--- p2s.v
+++ p2s.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.02.2023 13:40:19
-// Design Name: 
-// Module Name: p2s
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module uni_shift_reg(input start, clk, reset, input [7:0] in, output reg outgoing);
 reg [1:0] select;
@@ -26,13 +6,21 @@ localparam IDLE = 2'b00,
            LOAD = 2'b10,
            SEND = 2'b11;
 reg par;
+reg [4:0] count;
 reg [7:0] out;
-always@ (posedge clk)
+always@ (negedge clk)
 begin
-if(!start)
+if(reset) begin
+outgoing <= 1'bx; 
+out <= 0; 
+select <= 2'bxx;
+end
+else begin
+    if(!start)
 	   begin
 	   par <= in[7]^in[6]^in[5]^in[4]^in[3]^in[2]^in[1]^in[0];
 	   select<= IDLE;
+	   count <= 0;
 		case (select)
 			IDLE: begin outgoing <= 1'bx;
 			            select <= LOAD;
@@ -41,10 +29,22 @@ if(!start)
 			            select <= SEND;
 			      end
 			         
-			SEND: begin out <= {par,out[7:1]};
-			            outgoing <= out[0];
+			SEND: begin
+			            if(count != 4'b1000) begin
+			                 count <= count+1; 
+			                 outgoing <= out[0];
+			                 out <= {par,out[7:1]};
+			                 select <= SEND;
+			                 end
+			            else select <= IDLE;
 			      end
 		endcase
+		end
+		 else  begin
+                outgoing <= 1'bx; 
+                out <= 0; 
+                select <= 2'bxx;
+                end
 		end
 	   
 end
